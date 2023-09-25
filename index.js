6b49889ef897679610722840647d8b31a54b88ec
suryateja <suryateja1938102074@gmail.com>
2023-09-25 11:25:19 +0000

    commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+app.post("/login/", async (request, response) => {
+  const bookDetails = request.body;
+  const { username, password } = bookDetails;
+  const addBookQuery = `
+    INSERT INTO
+      per (username,password)
+    VALUES
+      (
+        '${username}',
+        '${password}'
+      );`;
+
+  const dbResponse = await db.run(addBookQuery);
+  response.send("success");
+});
