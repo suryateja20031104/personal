4d96039564f889b86437c89e45de4a6d98ce4941
suryateja <suryateja1938102074@gmail.com>
2023-09-25 11:51:28 +0000

    commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const dbPath = path.join(__dirname, "goodreads.db");
 
